591d75f7bc1d99139f02082fbcba979dcab3ee64
gaarangoa <gaarangoa@gmail.com>
2018-05-13 07:54:37 -0400

    update

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import os
 from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS
 from utils import tokenizerx
 import subprocess
 import re
 import unicodedata
 
-cmds = ['/src/bin/fastText-0.1.0/fasttext', 'predict-prob', '/src/sentiment/model/tweets.model.bin', '-', '5']
-interactive_model = subprocess.Popen( cmds, stdout=subprocess.PIPE, stdin=subprocess.PIPE )
+cmds = ['/src/bin/fastText-0.1.0/fasttext', 'predict-prob',
+        '/src/sentiment/model/tweets.model.bin', '-', '5']
+interactive_model = subprocess.Popen(
+    cmds, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+
 
 def clean_tweet(text):
-    txt = ' '.join(re.sub("(#[A-Za-z0-9]+)|(@[A-Za-z0-9_]+)|([\.\/\,\:\?\!\;])|(\w+:\/\/\S+)"," ",text.lower()).split())
+    txt = ' '.join(re.sub(
+        "(#[A-Za-z0-9]+)|(@[A-Za-z0-9_]+)|([\.\/\,\:\?\!\;])|(\w+:\/\/\S+)", " ", text.lower()).split())
     try:
         txt = unicode(txt, 'utf-8')
-    except (TypeError, NameError): # unicode is a default on python 3 
+    except (TypeError, NameError):  # unicode is a default on python 3
         pass
     txt = unicodedata.normalize('NFD', txt)
     txt = txt.encode('ascii', 'ignore')
@@ -22,26 +27,32 @@ def clean_tweet(text):
     txt = re.sub(r'([a-z][a-z])\1+', r'\1', txt)
     return txt
 
+
 def make_prediction(query):
     global interactive_model
     interactive_model.stdin.write(clean_tweet(query) + '\n')
-    i = " ".join(interactive_model.stdout.readline().strip().split('__label__')).split()
-    labels = {i[0]: float(i[1]), i[2]:float(i[3])}
+    i = " ".join(interactive_model.stdout.readline(
+    ).strip().split('__label__')).split()
+    labels = {i[0]: float(i[1]), i[2]: float(i[3])}
     return labels
 
+
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
 def info():
     return 'Tag analysis api'
 
+
 @app.route("/predict/", methods=['GET', 'POST'])
 def classify_text():
     data = request.get_json()
     sentence = data['sentence']
-    response = make_prediction( tokenizerx( sentence.lower() ) )
+    response = make_prediction(tokenizerx(sentence.lower()))
     return jsonify(response)
 
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
